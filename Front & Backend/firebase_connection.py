eaf571041a3cb88a2a77790cff8ac7650b9804ae
Muhammad Bilal <157012577+bilalsavagexd@users.noreply.github.com>
2024-05-11 20:17:03 +0500

    Updated firebase_connection.py

--- Front & Backend/firebase_connection.py
+++ Front & Backend/firebase_connection.py
@@ -19,18 +19,29 @@ db_connection = psycopg2.connect(
 # Function to sync data from SQL database to Firebase Realtime Database
 def sync_data():
     try:
+        # Sync data from books table
         cursor = db_connection.cursor()
         cursor.execute("SELECT * FROM books")
         books = cursor.fetchall()
 
-        # Convert SQL data to Firebase format and update Firebase database
-        firebase_ref = db.reference('books')
+        firebase_books_ref = db.reference('books')
         for book in books:
-            firebase_ref.child(str(book[0])).set({
+            firebase_books_ref.child(str(book[0])).set({
                 'title': book[1],
                 'author': book[2],
                 'edition': book[3],
                 'copies': book[4]
+            })
+
+        # Sync data from user_details table
+        cursor.execute("SELECT * FROM user_details")
+        user_details = cursor.fetchall()
+
+        firebase_users_ref = db.reference('user_details')
+        for user in user_details:
+            firebase_users_ref.child(str(user[0])).set({
+                'user_name': user[1],
+                'email': user[2]
             })
 
     except psycopg2.Error as error:
@@ -39,7 +50,7 @@ def sync_data():
 # Function to listen for real-time updates from Firebase Realtime Database
 def listen_for_updates():
     firebase_ref = db.reference('books')
-    firebase_ref.listen(lambda event: print(event.event_type, event.data))
+    firebase_ref.listen(lambda  event: print(event.event_type, event.data))
 
 # Call synchronization function
 sync_data()
